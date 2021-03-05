9605631aa312a27347c808bac807acbff70f0231
rpuropuu <de.rpuropuu@gmail.com>
2021-03-05 14:35:00 +0700

    change method output Add var.py

--- str_format.py
+++ str_format.py
@@ -1,5 +1,5 @@
 age = 37
 name = 'rpuropuu'
 
-print('Возраст {0} -- {1} лет.'.format(name, age))
-print('Почему {0} забавляется с этим Python?'.format(name))
+print('Возраст {} -- {} лет.'.format(name, age))
+print('Почему {} забавляется с этим Python?'.format(name))
