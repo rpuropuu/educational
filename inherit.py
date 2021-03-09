71175fe8e4cd3543f720782ddd914abfc9028918
rpuropuu <de.rpuropuu@gmail.com>
2021-03-09 20:07:26 +0700

    Added func_doc.py

--- inherit.py
+++ inherit.py
@@ -32,3 +32,9 @@ class Student(SchoolMember):
 
 t = Teacher('Mrs. Shrividya', 40, 30000)
 s = Student('Swaroop', 25, 75)
+
+print() # печатает пустую строку
+
+members = [t, s]
+for member in members:
+    member.tell() # работает как для преподавателя, так и для студента
