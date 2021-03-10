f208a4da6d3f6d9bc70ea1962040fd4ace3aed0b
rpuropuu <de.rpuropuu@gmail.com>
2021-03-10 12:43:25 +0700

    fix user_input.py

--- user_input.py
+++ user_input.py
@@ -2,13 +2,13 @@
 #    return text[::-1]
 
 
-def is_palindrome(text):
-    return text == text[::-1]
+#def is_palindrome(text):
+#    return text == text[::-1]
 
 
-something = input('Введите текст: ')
+text = input('Введите текст: ')
 
-if (is_palindrome(something)):
+if (text == text[::-1]):
     print('Да, это палиндром')
 else:
     print('Нет, это не палиндром')
