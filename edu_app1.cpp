e11717dd0e4c71a18f5215271f8f02ea2583e098
Grigorii Derun <79740197+rpuropuu@users.noreply.github.com>
2021-05-26 18:35:32 +0700

    Back to Cxx

---
+++ edu_app1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+//#include "pch.h"
+
+using namespace std;
+
+typedef struct {
+	char a;
+	int b;
+}VAR;
+
+int main(){
+	setlocale(LC_ALL, "rus");
+	VAR v = { '#', 5 };
+	VAR* p = &v;
+	cout << (*p).a << " " << (*p).b << endl;
+	p->a = '*';
+	p->b = -8;
+	cout << p->a << " " << p->b << endl;
+	return 0;
+}
